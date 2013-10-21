a3ed685eedc845bda526458b2079a79dbc1989e4
AK <ak@argumenti.ru>
2013-10-21 12:51:31 +0400

    add noncache headers

--- fpcache.php
+++ fpcache.php
@@ -7,84 +7,96 @@
 	if (!defined('FPCTIME'))
 		define('FPCTIME', 1800); //по умoлчанию 30 минут
 
-	if (!defined('FPCDIR'))
-		define('FPCDIR', $_SERVER['DOCUMENT_ROOT'].'/cache/');
-
 	
-	/* получаем URI */
-	if (isset($_SERVER['REQUEST_URI'])) {
-		$fpc_uri = trim($_SERVER['REQUEST_URI']); 
-		$fpc_uri = trim($fpc_uri,'/');
-	}
-	else
-		$fpc_uri = '';	
-
-	/* обработка GET */
-	if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] !== ''){ 
-		$fpc_get_line = str_replace('=','_', $_SERVER['QUERY_STRING']);
-		$fpc_query = mb_strpos($fpc_uri, '?');
-		$fpc_uri = mb_substr($fpc_uri, 0, $fpc_query);
-	}
-	else
-		$fpc_get_line = '';
-
-	if ($fpc_uri !== '') 
-		$fpcache = FPCDIR.'html/'.$fpc_uri;
-	else 	
-		$fpcache =  FPCDIR.'html/'.'index';  //определяем файл кеша гл страницы
-
-	if 	($fpc_get_line !== '')
-		$fpcache .= $fpc_get_line;
-
-	define('FPCFILE', $fpcache.'.html'); //текущий файл кеширования	
-
-	if (sizeof($_POST) == 0 or FPCTIME == -1) { //если пришли данные из формы кэш не нужен
-		
-		if (FPCTIME !== 0) {
-									
-	
-			if (FPCTIME == -1) // время кеширования для заголовков
-				$htime = 1200;
-			else
-				$htime = FPCTIME;
-
-			header("X-Accel-Expires: $htime");
-			header("Cache-Control: max-age=$htime");
-			header("Expires: ".gmdate("D, d M Y H:i:s", time()+$htime)." GMT");
-			
-						
-			if (file_exists(FPCFILE)) {
-								
-				if (FPCTIME == -1) {
-					echo file_get_contents(FPCFILE);
-					exit;
-				}
-			
-				$fp_endtime = filemtime(FPCFILE) + FPCTIME;
-
-				if ($fp_endtime > $_SERVER['REQUEST_TIME']){
-					$fp_rest = $fp_endtime - $_SERVER['REQUEST_TIME'];
-					fpc_headers($fp_rest);
-					echo file_get_contents(FPCFILE); //выводим файл кеша и обрываем выполнение скрипта
-					exit;
-				}	
-		
-			}
-
-		}
-
-	}
-
-
-	/*
-	* FPCACHE saved function
+	/**
+	* Cashing: headers, nginx, files 
 	*/
 
-	function fpc_headers($time){
-		header("X-Accel-Expires: ".$time); //nginx
-	} 
+	if ( FPCTIME !== 0 and sizeof($_POST) == 0 or FPCTIME == -1 ) {
+												
+		/**
+		* - nginx + headers
+		*/
+		if (FPCTIME == -1) 
+			$htime = 1200;
+		else
+			$htime = FPCTIME;
+
+		header("X-Accel-Expires: $htime");
+		header("Cache-Control: max-age=$htime");
+		header("Expires: ".gmdate("D, d M Y H:i:s", time()+$htime)." GMT");
+			
+
+		/**
+		* Файловый кеш
+		*/
+		if (!defined('FPCDIR'))
+			define('FPCDIR', $_SERVER['DOCUMENT_ROOT'].'/cache/');
+
+			
+		/**	определяем файл */
+		if (isset($_SERVER['REQUEST_URI'])) {
+			$fpc_uri = trim($_SERVER['REQUEST_URI']); 
+			$fpc_uri = trim($fpc_uri,'/');
+		}
+		else
+			$fpc_uri = '';	
+
+		/** обработка GET */
+		if (isset($_SERVER['QUERY_STRING']) && $_SERVER['QUERY_STRING'] !== ''){ 
+			$fpc_get_line = str_replace('=','_', $_SERVER['QUERY_STRING']);
+			$fpc_query = mb_strpos($fpc_uri, '?');
+			$fpc_uri = mb_substr($fpc_uri, 0, $fpc_query);
+		}
+		else
+			$fpc_get_line = '';
+
+		if ($fpc_uri !== '') 
+			$fpcache = FPCDIR.'html/'.$fpc_uri;
+		else 	
+			$fpcache =  FPCDIR.'html/'.'index';  //определяем файл кеша гл страницы
+
+		if 	($fpc_get_line !== '')
+			$fpcache .= $fpc_get_line;
+
+		define('FPCFILE', $fpcache.'.html'); //текущий файл кеширования
+
+		if (file_exists(FPCFILE)) { // если файловый кеш
+								
+			if (FPCTIME == -1) {
+				echo file_get_contents(FPCFILE);
+				exit;
+			}
+			
+			$fp_endtime = filemtime(FPCFILE) + FPCTIME;
+
+			if ($fp_endtime > $_SERVER['REQUEST_TIME']){
+				echo file_get_contents(FPCFILE); //выводим файл кеша и обрываем выполнение скрипта
+				exit;
+			}
+		}
+
+	}		
+
+	/**
+	* Обойдемся без кеширования
+	*/
+	else { 
+
+		/** не кешировать */
+		header("X-Accel-Expires: 0");
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); //Дата в прошлом 
+		header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1 
+		header("Pragma: no-cache"); // HTTP/1.1 
+		header("Last-Modified: ".gmdate("D, d M Y H:i:s")."GMT");
+
+	}
 
 
+
+	/**
+	* Блочное кеширование
+	*/
 	
 	// create cache directory
 	function fpc_dir($dir){
